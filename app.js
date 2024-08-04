aa598e65c933d521a8898310d69f308bed69e4f1
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-08-04 15:26:09 -0500

    Exercises complete

--- app.js
+++ app.js
@@ -5,37 +5,29 @@ console.log("EXERCISE 1:\n==========\n");
 
 for (let i = 1; i<=100; i++) 
 {
-    if (i % 2 == 0) {
+    if (i % 2 === 0) {
         continue;
     }
-    //else if (i % 2 == 0) {
-        console.log("Number is: " + i);
-    //}
+    
+    console.log("Number is: " + i);
 }
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+let result;
 for (let i = 1; i<=100; i++) {
-    if ((i % 3 == 0) && (i % 5) == 0){
-        console.log(i + " FIZZBUZZ")
-    }
-    else if (i % 3 == 0) {
-        console.log(i + " FIZZ");
-        continue;
-    }
-    else if (i % 5 == 0) {
-        console.log(i + " BUZZ");
-        continue;
-    }
-    
+    result = "";
+    if (i % 3 === 0) result += "FIZZ";
+    if (i % 5 === 0) result += "BUZZ";
+    if (result !== "") console.log(`${i}. ${result}`);
 }
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 let i = 1;
 while (i <=100) {
-    if (i % 2 == 1) {
+    if (i % 2 !== 0) {
         console.log(i);
     }
     i++;
@@ -43,16 +35,10 @@ while (i <=100) {
 
 i = 1;
 do {
-   // console.log("i is" + i);
-
-   if ((i % 3== 0) && (i % 5 ==0))
-        console.log(i + " NEWFIZZBUZZ")
-    else if (i % 3 == 0) {
-        console.log(i +  " NEWFIZZ");
-    }
-    else if (i % 5 == 0) {
-        console.log(i + " NEWBUZZ");
-    }
+    result = "NEW";
+    if (i % 3 === 0) result += "FIZZ";
+    if (i % 5 === 0) result += "BUZZ";
+    if (result !== "NEW") console.log(`${i}. ${result}`)
     i++;
 } while (i<=100)
 
@@ -66,7 +52,7 @@ let found = false;
 console.log("Upper bound is n which is " + n);
 
 for (let i=1; i<=n; i++) {
-    if (i == value) {
+    if (i === value) {
         console.log("Found the value " + i);
         found = true;
         break;
@@ -81,26 +67,18 @@ if (!found) {
 console.log("EXERCISE 5:\n==========\n");
 let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
 let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
-n = Math.round(Math.random() * (1000 - 1) + 1);
 let start = Math.round(Math.random() * (10 - 1) + 1);
+let end = Math.round(Math.random() * (1000 - 100) + 100); 
+result = "";
 
-console.log("Lower bound = " + start + ". Upper is " + n);
+
+console.log("Lower bound = " + start + ". Upper is " + end);
 console.log("fizzDivisor is " + fizzDivisor);
 console.log("buzzDivisor is " + buzzDivisor);
 
-for (let i = start; i<=n; i++) {
-    //old time debugging
-    //console.log ("in for, i is " + i);
-    
-    if ((i % fizzDivisor == 0) && (i % buzzDivisor == 0)) {
-        console.log("i is " + i  + " FIZZBUZZ");
-        continue;
-    }
-    else if (i % fizzDivisor == 0) {
-        console.log("i is " + i  + " FIZZ");
-        continue;
-    }
-    else if (i % buzzDivisor == 0) {
-        console.log("i is " + i + " BUZZ");
-    }
+for (let i = start; i<=end; i++) {
+    result = "";
+    if (i % fizzDivisor === 0) result += "FIZZ";
+    if (i % buzzDivisor === 0) result += "BUZZ";
+    if (result !== "") console.log(`${i}. ${result}`)
 }
